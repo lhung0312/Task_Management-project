8738d582bf49a0bdd115f6f508e46a52459a8bfb
lhung0312 <hung0372899952@gmail.com>
2024-06-04 13:26:30 +0700

    enable Cors

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const connection = require("./config/database");
+const cors = require("cors");
+
 const userRoutes = require("./routes/user.routes");
 const fileRoutes = require("./routes/file.routes");
 const customerRoutes = require("./routes/customer.routes");
 const projectRoutes = require("./routes/project.routes");
 const taskRoutes = require("./routes/task.routes");
-
 const authRoutes = require("./auth/auth.routes");
 const passport = require("./auth/localStrategy");
 const isAuthMiddleWare = require("./middlewares/isAuth.middleware");
 const fileUpload = require("express-fileupload");
-const port = process.env.PORT || 4000;
 
-//config req.body
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+const secretSession = process.env.SECRET_SESSION;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// default options
+
 app.use(fileUpload());
 
 app.use(
   require("express-session")({
-    secret: "keyboard cat",
+    secret: secretSession,
     resave: true,
     saveUninitialized: true,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(cors());
 
 app.use("/api/users", isAuthMiddleWare, userRoutes);
 app.use("/api/files", isAuthMiddleWare, fileRoutes);
